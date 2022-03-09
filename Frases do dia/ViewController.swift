2bc5380269188dd9d7ed44fa474b467c21c01a65
Hygor Martins <hygor.martins74@gmail.com>
2022-03-09 10:05:38 -0300

    Initial Commit

---
+++ Frases do dia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Frases do dia
+//
+//  Created by Hygor Martins on 09/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
